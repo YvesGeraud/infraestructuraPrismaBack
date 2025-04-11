7e246ecb622c261bc5aa7640f36dc6d6e6c6aef3
Flavio Lozano Morales <flavio.lozano@gmail.com>
2025-04-11 18:42:40 +0000

    Update 26 files
    - /src/controller/AuthController.ts
    - /src/controller/MunicipioController.ts
    - /src/controller/UnidadController.ts
    - /src/controller/UsuarioController.ts
    - /src/entity/CtDepartamento.ts
    - /src/entity/CtDireccion.ts
    - /src/entity/CtMunicipio.ts
    - /src/entity/CtUnidad.ts
    - /src/entity/CtUsuario.ts
    - /src/middleware/authMiddleware.ts
    - /src/migration/1740210720119-crearBase.ts
    - /src/migration/1740210767320-crearBase.ts
    - /src/routes/authRoutes.ts
    - /src/routes/municipioRoutes.ts
    - /src/routes/unidadRoutes.ts
    - /src/routes/usuarioRoutes.ts
    - /src/services/MunicipioService.ts
    - /src/services/UnidadService.ts
    - /src/services/UsuarioService.ts
    - /src/data-source.ts
    - /src/index.ts
    - /src/styles.css
    - /ormconfig.json
    - /package-lock.json
    - /package.json
    - /tsconfig.json

--- src/routes/municipioRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { MunicipioController } from "../controller/MunicipioController";
-
-const router = Router();
-
-// Obtener todos los municipios
-router.get("/", MunicipioController.getAll);
-
-export default router; 
